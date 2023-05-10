c18d509f6c89df896a7fd2452598a485ba02984c
Ranbut <Ranbut_Beater@protonmail.com>
2023-05-10 19:39:38 -0300

    feat: modality and hotel calculate ticket price

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -3,8 +3,8 @@ import { useState } from 'react';
 import styled from 'styled-components';
 
 export default function Payment() {
-  const [isRemote, setIsRemote] = useState(null);
-  const [haveHotel, sethaveHotel] = useState(null);
+  const [isRemote, setIsRemote] = useState(false);
+  const [haveHotel, sethaveHotel] = useState(false);
 
   function renderModalityOptions() {
     return (
@@ -44,11 +44,28 @@ export default function Payment() {
     }
   }
 
+  function calculateValue() {
+    const modalityValue = isRemote ? 100 : 250;
+    const hotelValue = !isRemote && haveHotel ? 250 : 0;
+
+    return modalityValue + hotelValue;
+  }
+
+  function renderResume() {
+    return (
+      <>
+        <StyledDescription>Fechado! O total ficou em <strong>R$ {calculateValue()}</strong>. Agora é só confirmar:</StyledDescription>
+        <ButtonConfirmation>RESERVAR INGRESSO</ButtonConfirmation>
+      </>
+    );
+  }
+
   return (
     <>
       <StyledTypography variant="h4">Ingresso e pagamento</StyledTypography>
       {renderModalityOptions()}
       {renderHotelOptions()}
+      {renderResume()}
     </>
   );
 }
@@ -83,6 +100,22 @@ const SelectButtonOption = styled.button`
   background: ${props => (props.selected ? '#FFEED2' : '#CECECE')};
   border: 1px solid #CECECE;
   border-radius: 20px;
+`;
+
+const ButtonConfirmation = styled.button`
+  width: 162px;
+  height: 37px;
+
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 16px;
+  text-align: center;
+
+  background: #E0E0E0;
+  border: 1px solid #E0E0E0;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 4px;
 `;
 
 const OptionName = styled.p`
