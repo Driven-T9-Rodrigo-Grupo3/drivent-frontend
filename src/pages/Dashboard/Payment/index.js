7c21d303672560a5c92f0fa1cebc01c0ac8f983a
Ranbut <Ranbut_Beater@protonmail.com>
2023-05-11 14:26:18 -0300

    fix: disable renderPaymentConfirmation by default

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -107,7 +107,6 @@ export default function Payment() {
       {renderModalityOptions()}
       {renderHotelOptions()}
       {renderResume()}
-      {renderPaymentConfirmation()}
       {renderNoEnrollment()}
     </>
   );
