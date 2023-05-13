6495799da934d020e6332b9473897f1a3ce50560
Alexandre Dresch <z3r0pc@protonmail.com>
2023-05-12 23:44:33 -0300

    feat: post ticket update

--- src/services/ticketApi.js
+++ src/services/ticketApi.js
@@ -8,3 +8,13 @@ export async function getUserTicket(token) {
   });
   return response.data;
 }
+
+export async function postTicket(body, token) {
+  const response = await api.post('/tickets', body, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
+}
